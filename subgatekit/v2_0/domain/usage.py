adb8a3cbbcccd67292f90d70310058fefa7ff924
barma <barmatey@gmail.com>
2025-02-11 11:46:14 +0300

    new_sub

--- subgatekit/v2_0/domain/usage.py
+++ subgatekit/v2_0/domain/usage.py
@@ -1,9 +1,9 @@
 import datetime
-from typing import Self
+from typing import Self, Optional
 
-from subgatekit.domain.cycle import Cycle
-from subgatekit.domain.enums import Period
 from subgatekit.utils import get_current_datetime, Number
+from subgatekit.v2_0.domain.enums import Period
+from subgatekit.v2_0.domain.validators import TypeValidator, EnumValidator, raise_errors_if_necessary
 
 
 class UsageRate:
@@ -15,14 +15,35 @@ class UsageRate:
             available_units: float,
             renew_cycle: Period,
     ):
+        self._validate(title, code, unit, available_units, renew_cycle)
         self.title = title
         self.code = code
         self.unit = unit
         self.available_units = available_units
-        self.renew_cycle = Cycle.from_code(renew_cycle)
+        self.renew_cycle = renew_cycle
+
+    @staticmethod
+    def _validate(
+            title: str,
+            code: str,
+            unit: str,
+            available_units: float,
+            renew_cycle: Period,
+    ):
+        validators = [
+            TypeValidator("UsageRate.title", title, str),
+            TypeValidator("UsageRate.code", code, str),
+            TypeValidator("UsageRate.unit", unit, str),
+            TypeValidator("UsageRate.available_units", available_units, Number),
+            EnumValidator("UsageRate.renew_cycle", renew_cycle, Period),
+        ]
+        errors = []
+        for validator in validators:
+            errors.extend(validator.validate().parse_errors())
+        raise_errors_if_necessary(errors)
 
 
-class Usage(UsageRate):
+class Usage:
     def __init__(
             self,
             title: str,
@@ -33,8 +54,13 @@ class Usage(UsageRate):
             used_units: Number = 0,
             last_renew: datetime.datetime = None,
     ):
-        super().__init__(title, code, unit, available_units, renew_cycle)
+        self._validate(title, code, unit, available_units, renew_cycle, used_units, last_renew)
+        self.title = title
+        self.code = code
+        self.unit = unit
+        self.available_units = available_units
         self.used_units = used_units
+        self.renew_cycle = renew_cycle
         self.last_renew = last_renew if last_renew else get_current_datetime()
 
     @classmethod
@@ -45,8 +71,32 @@ class Usage(UsageRate):
             unit=usage_rate.unit,
             available_units=usage_rate.available_units,
             used_units=0,
-            renew_cycle=usage_rate.renew_cycle.code,
+            renew_cycle=usage_rate.renew_cycle,
         )
 
     def increase(self, delta: float) -> None:
         self.used_units += delta
+
+    @staticmethod
+    def _validate(
+            title: str,
+            code: str,
+            unit: str,
+            available_units: float,
+            renew_cycle: Period,
+            used_units: Number,
+            last_renew: Optional[datetime.datetime],
+    ):
+        validators = [
+            TypeValidator("UsageRate.title", title, str),
+            TypeValidator("UsageRate.code", code, str),
+            TypeValidator("UsageRate.unit", unit, str),
+            TypeValidator("UsageRate.available_units", available_units, Number),
+            EnumValidator("UsageRate.renew_cycle", renew_cycle, Period),
+            TypeValidator("UsageRate.used_units", used_units, Number),
+            TypeValidator("UsageRate.last_renew", last_renew, datetime.datetime, True),
+        ]
+        errors = []
+        for validator in validators:
+            errors.extend(validator.validate().parse_errors())
+        raise_errors_if_necessary(errors)
