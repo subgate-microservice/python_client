89ad1cd96ae68f4afd5338d887c944188dc6170e
barma <barmatey@gmail.com>
2025-02-22 20:02:16 +0300

    processing

--- tests/test_events.py
+++ tests/test_events.py
@@ -10,9 +10,9 @@ from snippets.tests.client import get_client
 from subgatekit import Webhook, EventCode, Plan, Period
 
 
-class Event[T](BaseModel):
+class Event(BaseModel):
     type: str
-    event_code: str
+    event_code: EventCode
     occurred_at: AwareDatetime
     payload: dict
 
@@ -24,16 +24,41 @@ client = get_client()
 app = FastAPI()
 
 
+class EventStore:
+    def __init__(self):
+        self._events = {}
+        self._counter = {}
+
+    def add(self, event: Event):
+        self._events[event.event_code] = event
+        if event.event_code not in self._counter:
+            self._counter[event.event_code] = 0
+        self._counter[event.event_code] += 1
+
+    def check(self, code: EventCode, count: int):
+        assert self._events.get(code) is not None
+        assert self._counter.get(code, -1) == count
+
+    def clear(self):
+        self._events = {}
+        self._counter = {}
+
+
+event_store = EventStore()
+
+
 @app.post("/event-handler")
 async def event_handler(event: Event) -> str:
-    print(event)
+    event_store.add(event)
     return "OK"
 
 
 def create_webhooks():
     client.webhook_client().delete_all()
-    hook = Webhook(event_code=EventCode.PlanCreated, target_url=f"http://{HOST}:{PORT}/event-handler")
-    client.webhook_client().create(hook)
+    for code in EventCode:
+        code: EventCode
+        hook = Webhook(event_code=code, target_url=f"http://{HOST}:{PORT}/event-handler")
+        client.webhook_client().create(hook)
 
 
 async def run_fastapi():
@@ -52,13 +77,20 @@ def core_logic():
 @pytest_asyncio.fixture(scope="module")
 async def fastapi_server():
     server, task = await run_fastapi()
-    create_webhooks()  # Создаем вебхуки
+    create_webhooks()
     yield
     await server.shutdown()
     task.cancel()
 
 
+@pytest.fixture(autouse=True)
+def clear_events():
+    event_store.clear()
+
+
 @pytest.mark.asyncio
-async def test_webhooks(fastapi_server):
-    core_logic()
-    await asyncio.sleep(10)
+async def test_plan_created(fastapi_server):
+    plan = Plan("Business", 100, "USD", Period.Annual)
+    client.plan_client().create(plan)
+    await asyncio.sleep(1)
+    event_store.check(EventCode.PlanCreated, 1)
