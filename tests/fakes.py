c0a0fb3292d9548b009aa19e41e9ecf1ed08ca7a
barma <barmatey@gmail.com>
2025-02-24 15:08:27 +0300

    processing

---
+++ tests/fakes.py
@@ -0,0 +1,49 @@
+import pytest
+
+from subgatekit import Subscription, Plan, Period, SubscriptionStatus, Discount, UsageRate
+from subgatekit.utils import get_current_datetime
+
+
+@pytest.fixture()
+def simple_subscription(sync_client):
+    plan = Plan("Business", 100, "USD", Period.Monthly)
+    sub = Subscription.from_plan(plan, "AnyID", )
+    sub = sync_client.subscription_client().create_then_get(sub)
+    yield sub
+
+
+@pytest.fixture()
+def paused_subscription(sync_client):
+    plan = Plan("Business", 100, "USD", Period.Monthly)
+    sub = Subscription.from_plan(plan, "AnyID", )
+    sub.pause()
+    sub: Subscription = sync_client.subscription_client().create_then_get(sub)
+    assert sub.status == SubscriptionStatus.Paused
+    yield sub
+
+
+@pytest.fixture()
+def subscription_with_usages(sync_client):
+    plan = Plan("Business", 100, "USD", Period.Monthly)
+    plan.usage_rates.add(UsageRate("ApiCall", "api_call", "request", 100, Period.Monthly))
+    sub = Subscription.from_plan(plan, "AnyId")
+    sub = sync_client.subscription_client().create_then_get(sub)
+    yield sub
+
+
+@pytest.fixture()
+def subscription_with_discounts(sync_client):
+    plan = Plan("Business", 100, "USD", Period.Monthly)
+    sub = Subscription.from_plan(plan, "AnyID", )
+    sub.discounts.add(Discount("First", "first", 0.2, get_current_datetime()))
+    sub.discounts.add(Discount("Second", "second", 0.2, get_current_datetime()))
+    sub = sync_client.subscription_client().create_then_get(sub)
+    yield sub
+
+
+@pytest.fixture()
+def subscription_with_fields(sync_client):
+    plan = Plan("Business", 100, "USD", Period.Monthly)
+    sub = Subscription.from_plan(plan, "AnyID", fields={"Hello": "World!"})
+    sub = sync_client.subscription_client().create_then_get(sub)
+    yield sub
--- tests/test_subscription.py
+++ tests/test_subscription.py
@@ -7,82 +7,36 @@ from subgatekit.enums import Period, SubscriptionStatus
 from subgatekit.exceptions import ActiveStatusConflict
 from subgatekit.utils import get_current_datetime
 from tests.conftest import client, wrapper
+from tests.fakes import (simple_subscription, subscription_with_usages, subscription_with_discounts,
+                         paused_subscription, subscription_with_fields)
 
 
 class TestGetSubscription:
     @pytest.mark.asyncio
-    async def test_get_test_get_one_by_id(self, client):
-        # Before
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        sub = Subscription.from_plan(plan, "AnyID")
-        await wrapper(client.subscription_client().create(sub))
-
-        # Test
-        real = await wrapper(client.subscription_client().get_by_id(sub.id))
-        assert real.id == sub.id
+    async def test_get_test_get_one_by_id(self, client, simple_subscription):
+        real = await wrapper(client.subscription_client().get_by_id(simple_subscription.id))
+        assert real.id == simple_subscription.id
 
     @pytest.mark.asyncio
-    async def test_get_one_with_usages(self, client):
-        # Before
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        plan.usage_rates.add(UsageRate("First", "first", "GB", 100, Period.Monthly))
-        plan.usage_rates.add(UsageRate("Second", "sec", "GB", 100, Period.Monthly))
-        sub = Subscription.from_plan(plan, "AnyID")
-        await wrapper(client.subscription_client().create(sub))
-
-        # Test
-        real: Subscription = await wrapper(client.subscription_client().get_by_id(sub.id))
-        assert real.id == sub.id
-        assert len(real.usages.get_all()) == 2
-        assert real.usages.get("sec").title == "Second"
-        assert real.usages.get("first").title == "First"
+    async def test_get_one_with_usages(self, client, subscription_with_usages):
+        real: Subscription = await wrapper(client.subscription_client().get_by_id(subscription_with_usages.id))
+        assert real.id == subscription_with_usages.id
+        assert len(real.usages) == 1
 
     @pytest.mark.asyncio
-    async def test_get_one_with_discounts(self, client):
-        # Before
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        plan.discounts.add(Discount("First", "first", 0.3, get_current_datetime()))
-        plan.discounts.add(Discount("Second", "sec", 0.8, get_current_datetime()))
-        sub = Subscription.from_plan(plan, "AnyID")
-        await wrapper(client.subscription_client().create(sub))
-
-        # Test
-        real: Subscription = await wrapper(client.subscription_client().get_by_id(sub.id))
-        assert real.id == sub.id
-        assert len(real.discounts.get_all()) == 2
-        assert real.discounts.get("sec").title == "Second"
-        assert real.discounts.get("first").title == "First"
+    async def test_get_one_with_discounts(self, client, subscription_with_discounts):
+        real: Subscription = await wrapper(client.subscription_client().get_by_id(subscription_with_discounts.id))
+        assert real.id == subscription_with_discounts.id
+        assert len(real.discounts) == 2
 
     @pytest.mark.asyncio
-    async def test_get_one_with_fields(self, client):
-        # Before
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        sub = Subscription.from_plan(plan, "AnyID", fields={"Hello": "World!"})
-        await wrapper(client.subscription_client().create(sub))
-
-        # Test
-        real: Subscription = await wrapper(client.subscription_client().get_by_id(sub.id))
-        assert real.id == sub.id
+    async def test_get_one_with_fields(self, client, subscription_with_fields):
+        real: Subscription = await wrapper(client.subscription_client().get_by_id(subscription_with_fields.id))
+        assert real.id == subscription_with_fields.id
         assert real.fields == {"Hello": "World!"}
 
 
 class TestUpdateSubscription:
-    @pytest.fixture()
-    def simple_subscription(self, sync_client):
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        sub = Subscription.from_plan(plan, "AnyID", )
-        sync_client.subscription_client().create(sub)
-        yield sub
-
-    @pytest.fixture()
-    def paused_subscription(self, sync_client):
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        sub = Subscription.from_plan(plan, "AnyID", )
-        sub.pause()
-        sub: Subscription = sync_client.subscription_client().create_then_get(sub)
-        assert sub.status == SubscriptionStatus.Paused
-        yield sub
-
     @pytest.fixture()
     def two_subs_for_one_subscriber(self, sync_client):
         plan = Plan("Business", 100, "USD", Period.Monthly)
@@ -95,23 +49,6 @@ class TestUpdateSubscription:
         assert sub2.status == SubscriptionStatus.Paused
 
         yield sub1, sub2
-
-    @pytest.fixture()
-    def subscription_with_usages(self, sync_client):
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        plan.usage_rates.add(UsageRate("ApiCall", "api_call", "request", 100, Period.Monthly))
-        sub = Subscription.from_plan(plan, "AnyId")
-        sync_client.subscription_client().create(sub)
-        yield sub
-
-    @pytest.fixture()
-    def subscription_with_discounts(self, sync_client):
-        plan = Plan("Business", 100, "USD", Period.Monthly)
-        sub = Subscription.from_plan(plan, "AnyID", )
-        sub.discounts.add(Discount("First", "first", 0.2, get_current_datetime()))
-        sub.discounts.add(Discount("Second", "second", 0.2, get_current_datetime()))
-        sync_client.subscription_client().create(sub)
-        yield sub
 
     @pytest.mark.asyncio
     async def test_pause_subscription(self, client, simple_subscription):
